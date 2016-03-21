470c6802bc850ac604e24a2f67764d5a659b81c4
Rafael-Barreira <rafael.spam.barreira@gmail.com>
2016-03-21 16:40:29 +0000

    imprime os metódos que são chamados num cero método

--- Test.java
+++ Test.java
@@ -1,5 +1,18 @@
+import java.io.UnsupportedEncodingException;
 public class Test {
-	public static void main (String[] args) {
-		System.out.println("Hello...is it me you're looking for?");
+	/*public static void main (String[] args)throws UnsupportedEncodingException {
+		Integer x = 1;
+		x.intValue();
+		System.out.println("POOOOOOOOOOP");
+		lel();
+	}*/
+	
+	public static String lel()throws UnsupportedEncodingException{
+		String a = "2";
+	    a.getBytes();
+	    a.getBytes("we");
+	    System.out.println(a);
+	    int t = Integer.parseInt(a);
+	    return a;
 	}
 }
--- src/ist/meic/pa/BoxingProfiler.java
+++ src/ist/meic/pa/BoxingProfiler.java
@@ -1,5 +1,6 @@
 package ist.meic.pa;
 
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -10,23 +11,60 @@ import javassist.CtField;
 import javassist.CtMethod;
 import javassist.CtNewMethod;
 import javassist.NotFoundException;
+import javassist.expr.ExprEditor;
+import javassist.expr.MethodCall;
 
 public class BoxingProfiler {
 	
 	//the name of another Java program and 
 	//the arguments that should be provided 
 	//to that program
-	public static void main (String[] args) throws NotFoundException, CannotCompileException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		if (args.length != 2) {
-			System.err.println("Usage: <Class to Profile> <method>");
-			System.exit(1);
-			} else {
-				ClassPool pool = ClassPool.getDefault();
-				CtClass ctClass = pool.get(args[0]);
-				Class<?> rtClass = ctClass.toClass();
-				Method main = rtClass.getMethod("main", args.getClass());
-				String[] restArgs = new String[args.length - 2];
-				main.invoke(null, new Object[] { restArgs });
-			}
+	
+	static void memoize(CtClass ctClass, CtMethod[] ctMethod) throws ClassNotFoundException, NotFoundException, CannotCompileException {
+		CtMethod vInteger = ctMethod[0];
+		for(int i = 0; i < ctMethod.length; i++) {
+			//ctMethod[i].get
+		}
 	}
+	
+	public static void main (String[] args)throws NotFoundException, CannotCompileException, IOException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, ClassNotFoundException {
+		System.out.println("I'm in!");
+		if (args.length < 2) {
+			System.err.println("Usage: java Memoize <class> <method>");
+			System.exit(1);
+		} else {
+			
+			ClassPool pool = ClassPool.getDefault();
+			CtClass ctClass = pool.getCtClass(args[0]);
+			//memoize(ctClass, ctClass.getMethods());
+			
+			/*Class<?> rtClass = ctClass.toClass();
+			Method main = rtClass.getMethod(args[1], args.getClass());*/
+			CtMethod method = ctClass.getDeclaredMethod("lel");
+			method.instrument(
+			        new ExprEditor() {
+			            public void edit(MethodCall m)
+			                          throws CannotCompileException
+			            {
+			                System.out.println(m.getClassName() + "." + m.getMethodName() + " " + m.getSignature());
+			            }
+			        });
+			/*String[] restArgs = new String[args.length - 2];
+			System.arraycopy(args, 2, restArgs, 0, restArgs.length);*/
+			//main.invoke(null, new Object[] { restArgs });
+			
+			/*ClassPool pool = ClassPool.getDefault();
+			
+			CtClass ctClass = pool.get(args[0]);
+			
+			memoize(ctClass, ctClass.getDeclaredMethods());
+			
+			Class<?> rtClass = ctClass.toClass();
+			Method main = rtClass.getMethod("main", args.getClass());
+			String[] restArgs = new String[args.length - 2];
+			System.arraycopy(args, 2, restArgs, 0, restArgs.length);
+			main.invoke(null, new Object[] { restArgs });*/
+		}
+	}
+	
 }
