bbe50027cbec7d775b2feb24c6d92e59cece5c6b
Loureiro <pedro.loureiro.93@gmail.com>
2016-03-23 11:37:07 +0000

    Almost There
    BoxingProfiler implements global counters and prints them at the end.
    PROBLEM: Bad injection of counter++. Line numbers arent consistent

--- Test.java
+++ Test.java
@@ -1,21 +1,21 @@
 import java.io.UnsupportedEncodingException;
 public class Test {
-	
+
 	private static long sumOfIntegerUptoN(Integer n) {
 		Long sum = 0L;
 		for (int i = 0; i < n; i++) {
-		sum += i;
+			sum += i;
 		}
 		return sum;
-		}
-		private static long printSum(Long n) {
+	}
+	private static long printSum(Long n) {
 		System.out.println("Sum: " + n);
 		return n;
-		}
-		public static void main(String[] args) {
+	}
+	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
 		printSum(sumOfIntegerUptoN(100000000));
 		long end = System.currentTimeMillis();
 		System.out.println("Time: " + (end - start));
-		}
+	}
 }
--- src/ist/meic/pa/BoxingProfiler.java
+++ src/ist/meic/pa/BoxingProfiler.java
@@ -3,12 +3,13 @@ package ist.meic.pa;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtField;
-import javassist.CtMethod;
+import javassist.CtMethod; 
 import javassist.CtNewMethod;
 import javassist.NotFoundException;
 import javassist.expr.ExprEditor;
@@ -24,14 +25,23 @@ public class BoxingProfiler {
 
 	}
 	
-	static String methodParser(String classmethod, String methodname) {
-		
-		if(methodname.equals("valueOf")) {
-			return "boxed "+ classmethod;			
-		} else if(methodname.endsWith("Value")) {
-			return "unboxed " + classmethod;
+	static String methodParser(String classmethod, String methodname, String mainmethod) {
+		//System.out.println(classmethod + " " + methodname);
+		if(classmethod.startsWith("java.lang.")) {			
+			if(methodname.equals("valueOf")) {
+				String[] type = classmethod.split("[.]");
+				String counter = mainmethod + type[2] + "Boxing";
+				System.out.println(counter);
+				return counter;
+				//return "boxed "+ classmethod;			
+			} else if(methodname.endsWith("Value")) {
+				String[] type = classmethod.split("[.]");
+				String counter = mainmethod + type[2] + "Unboxing";
+				System.out.println(counter);
+				return counter;
+				//return "unboxed " + classmethod;
+			} else return null;
 		} else return null;
-		
 	}
 	
 	public static void main (String[] args)throws NotFoundException, CannotCompileException, IOException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, ClassNotFoundException {
@@ -44,22 +54,44 @@ public class BoxingProfiler {
 			CtClass ctClass = pool.getCtClass(args[0]);
 			
 			for(CtMethod methods: ctClass.getDeclaredMethods()) {
-				String pmethod = methods.getLongName();
-			
+				String pmethod = methods.getName();
+				ArrayList<String> counterList = new ArrayList<String>();
+				
 				methods.instrument(
 			        new ExprEditor() {
 			            public void edit(MethodCall m)
 			                          throws CannotCompileException
 			            {
-			            	String autoBoxing = methodParser(m.getClassName(), m.getMethodName());
-			            	if(autoBoxing!=null){
+			            	
+			            	String counter = methodParser(m.getClassName(), m.getMethodName(), pmethod);
+			            	if(counter!=null){
 			            		//System.out.println(pmethod + " " + m.getClassName() + "." + m.getMethodName() + " ");
-			            		System.out.println(pmethod + " " + autoBoxing);
-			            		System.out.println("#######################");
+			            		//System.out.println(pmethod + " " + autoBoxing);
+			            		if(!counterList.contains(counter)) {
+			            			counterList.add(counter);
+			            			CtField ctField = CtField.make("public static int " + counter + " =0;", ctClass);
+			    					ctClass.addField(ctField);
+			            		}		
+			            		System.out.println(m.getLineNumber());
+			            		
+			            		methods.insertAt(m.getLineNumber(), counter+"++;");
+			            		//int index = m.getLineNumber();
+			            		//methods.insertAt(index, counter+"++;");
+			            		//System.out.println("#######################");
 			            	}            
 			            }
 			        });
+				for(String c : counterList) {
+					//methods.insertBefore("int " + c + "=0;"+"System.out.println(\""+ c +": \" + "+ c +");");
+					//CtField cc = new CtField(CtClass.intType, c, ctClass);
+					//ctClass.addField(cc, "0");
+					ctClass.getDeclaredMethod("main").insertAfter("System.out.println(\""+ c +": \" + "+ c +");");
+				}
 			}
+			
+			Class<?> rtClass = ctClass.toClass();
+			Method main = rtClass.getMethod("main", args.getClass());
+			main.invoke(null, new Object[] { args });
 			
 			/*FIXME*/ 
 			//memoize(ctClass, ctClass.getMethods());
