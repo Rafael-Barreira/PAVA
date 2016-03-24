f551c65659d5034d61141743fe27de763b5d5099
Rafael-Barreira <rafael.spam.barreira@gmail.com>
2016-03-24 15:05:59 +0000

    If e Else if for days

--- src/ist/meic/pa/BoxingProfiler.java
+++ src/ist/meic/pa/BoxingProfiler.java
@@ -28,7 +28,6 @@ public class BoxingProfiler {
 	//to that program
 	
 	static String counterCreator(String classmethod, String methodname, String mainmethod) {
-		//System.out.println(classmethod + " " + methodname);
 		if(classmethod.startsWith("java.lang.")) {			
 			if(methodname.equals("valueOf")) {
 				String[] type = classmethod.split("[.]");
@@ -37,13 +36,65 @@ public class BoxingProfiler {
 				return counter;
 				//return "boxed "+ classmethod;			
 			} else if(methodname.endsWith("Value")) {
-				String[] type = classmethod.split("[.]");
-				String counter = mainmethod + "_" + type[2] + "_" + "unboxed";
-				//System.out.println(counter);
-				return counter;
-				//return "unboxed " + classmethod;
+				if(methodname.startsWith("int") && classmethod.endsWith("Integer")){
+					String[] type = classmethod.split("[.]");
+					String counter = mainmethod + "_" + type[2] + "_" + "unboxed";
+					//System.out.println(counter);
+					return counter;
+					//return "unboxed " + classmethod;
+				}
+				else if(methodname.startsWith("boolean") && classmethod.endsWith("Boolean")){
+					String[] type = classmethod.split("[.]");
+					String counter = mainmethod + "_" + type[2] + "_" + "unboxed";
+					//System.out.println(counter);
+					return counter;
+					//return "unboxed " + classmethod;
+				}
+				else if(methodname.startsWith("byte") && classmethod.endsWith("Byte")){
+					String[] type = classmethod.split("[.]");
+					String counter = mainmethod + "_" + type[2] + "_" + "unboxed";
+					//System.out.println(counter);
+					return counter;
+					//return "unboxed " + classmethod;
+				}
+				else if(methodname.startsWith("char") && classmethod.endsWith("Character")){
+					String[] type = classmethod.split("[.]");
+					String counter = mainmethod + "_" + type[2] + "_" + "unboxed";
+					//System.out.println(counter);
+					return counter;
+					//return "unboxed " + classmethod;
+				}
+				else if(methodname.startsWith("float") && classmethod.endsWith("Float")){
+					String[] type = classmethod.split("[.]");
+					String counter = mainmethod + "_" + type[2] + "_" + "unboxed";
+					//System.out.println(counter);
+					return counter;
+					//return "unboxed " + classmethod;
+				}
+				else if(methodname.startsWith("long") && classmethod.endsWith("Long")){
+					String[] type = classmethod.split("[.]");
+					String counter = mainmethod + "_" + type[2] + "_" + "unboxed";
+					//System.out.println(counter);
+					return counter;
+					//return "unboxed " + classmethod;
+				}
+				else if(methodname.startsWith("short") && classmethod.endsWith("Short")){
+					String[] type = classmethod.split("[.]");
+					String counter = mainmethod + "_" + type[2] + "_" + "unboxed";
+					//System.out.println(counter);
+					return counter;
+					//return "unboxed " + classmethod;
+				}
+				else if(methodname.startsWith("double") && classmethod.endsWith("Double")){
+					String[] type = classmethod.split("[.]");
+					String counter = mainmethod + "_" + type[2] + "_" + "unboxed";
+					//System.out.println(counter);
+					return counter;
+					//return "unboxed " + classmethod;
+				}
 			} else return null;
 		} else return null;
+		return null;
 	}
 	
 	public static void main (String[] args)throws NotFoundException, CannotCompileException, IOException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, ClassNotFoundException {
@@ -60,13 +111,11 @@ public class BoxingProfiler {
 				final ArrayList<String> counterList = new ArrayList<String>();
 				Map<String, String> hashMap = new HashMap<String, String>();
 				
-				
 				methods.instrument(
 			        new ExprEditor() {
 			            public void edit(MethodCall m)
 			                          throws CannotCompileException
 			            {
-			            	
 			            	String counter = counterCreator(m.getClassName(), m.getMethodName(), pmethod);
 			            	if(counter!=null){
 			            		//System.out.println(pmethod + " " + m.getClassName() + "." + m.getMethodName() + " ");
@@ -117,7 +166,9 @@ public class BoxingProfiler {
 					insert1= methodFullName + space + action + space;
 					insert2=space+type;
 					hashMap.put(key, insert1 + insert2);
-					ctClass.getDeclaredMethod("main").insertAfter("System.out.println(\""+ methodFullName + space + action + space + "\" + "+ c +" + \""+space+type+"\");");	
+					ctClass.getDeclaredMethod("main").insertAfter(
+							"if("+ c +" != 0 ){"+
+							"System.out.println(\""+ methodFullName + space + action + space + "\" + "+ c +" + \""+space+type+"\");}");	
 				}
 				//DOESNT ORDER MY ALPHABETICAL ORDER
 				//Map reverseOrderedMap = new TreeMap(Collections.reverseOrder());
